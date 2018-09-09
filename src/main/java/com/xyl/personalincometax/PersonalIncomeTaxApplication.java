e6ad9647a6ef033f6630c849d1a5f8d5241c306c
369702770@qq.com <xun369702770S>
2018-09-09 10:13:08 +0800

    first commit

---
+++ src/main/java/com/xyl/personalincometax/PersonalIncomeTaxApplication.java
@@ -0,0 +1,12 @@
+package com.xyl.personalincometax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalIncomeTaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalIncomeTaxApplication.class, args);
+	}
+}
